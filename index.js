a5a244e1c082533059a04095fe8b91fc5323b9c5
Tomas Olalde <olaldetomas@gmail.com>
2023-01-16 12:38:01 -0300

    refactor dolar hoy

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { default: axios } = require('axios');
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore, FieldValue } = require('firebase-admin/firestore');
 const config = require('./config');
+const cheerio = require('cheerio');
 
 // APP
 const PORT = config.PORT;
@@ -23,11 +24,12 @@ const web = new WebClient(config.SLACK_BOT_TOKEN);
 // ROUTES
 app.post('/bot', async function (req, res) {
   const result = await axios.get(config.DOLAR_URL);
+  const $ = cheerio.load(result.data);
+  const valores = $('div.data__valores p').text();
+  var numbers = valores.match(/\d+\.\d+/g);
 
-  const actualSellPrice = parseInt(result.data.venta.split(',')[0]);
-  const actualBuyPrice = parseInt(result.data.compra.split(',')[0]);
-  const variation = result.data.variacion;
-  const classVariation = result.data['class-variacion'];
+  const actualBuyPrice = parseInt(numbers[0]);
+  const actualSellPrice = parseInt(numbers[1]);
 
   const snapshot = await db
     .collection('historyPrice')
@@ -44,10 +46,7 @@ app.post('/bot', async function (req, res) {
         createdAt: FieldValue.serverTimestamp()
       });
 
-      const arrowVariation =
-        classVariation === 'up' ? ':face_with_rolling_eyes:' : ':pensive:';
-
-      const text = `>:money_with_wings:  Dolar Blue  :money_with_wings:\n>Venta ........... $ *${actualSellPrice}*\n>Compra ....... $ ${actualBuyPrice}\n>Variación ..... ${arrowVariation} *${variation}*`;
+      const text = `>:money_with_wings:  Dolar Blue  :money_with_wings:\n>Venta ........... $ *${actualSellPrice}*\n>Compra ....... $ ${actualBuyPrice}`;
 
       await web.chat.postMessage({
         text: text,
